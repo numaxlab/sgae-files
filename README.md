2bd5040749ea7971f9d362a44849fd6d692b0624
Adrián P. Blunier <adrian@numax.org>
2017-07-04 17:46:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SGAE Files Component
+
+Componente para lectura y escritura de ficheros de envío de información de exhibición cinematográfica desagregada a la SGAE.
+
+## Instalación
+
+Este paquete es instalable y autocargable a través de Composer:
+
+```$ composer require numaxlab/sgae-files```
---
+++ SgaeFile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NumaxLab\Sgae;
+
+
+class SgaeFile
+{
+    private $sessions;
+
+    private $works;
+
+    public function __construct()
+    {
+
+    }
+
+    public static function parse($input, $eol = PHP_EOL)
+    {
+
+    }
+
+    public function dump($eol = PHP_EOL)
+    {
+
+    }
+}
